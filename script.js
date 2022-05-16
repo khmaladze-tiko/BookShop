7a614b2e172688fec09d7ee42c01c21fc2eb72c2
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2022-05-17 00:59:02 +0400

    feat: add header in js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-fetch('../books.json') //path to the file with json data
+fetch('./assets/books.json')
         .then(response => {
             return response.json();
         })
         .then(data => {
             console.log(data);
         });
+
+        const main = document.createElement('main');
+        const body = document.body;
+        body.appendChild(main);
+
+        // in main = h1, div ( div(img), div(h3,h2,h4, div(a, buttom)) )
+
+        const header = document.createElement('h1');
+        main.appendChild(header);
+        header.textContent = 'Book Catalog';
+        header.classList.add('main-title')
+
